b06cc6ad9169883a5329c51d14c6232b52bc522a
jack chen <1330582146@qq.com>
2025-10-29 14:24:18 +0800

    10.29下 教师代码

--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -1,14 +1,12 @@
-// pages/home/index.js
+// pages/order/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    fileUrl: 'cloud://cloud1-4gm1714uc326e5bf.636c-cloud1-4gm1714uc326e5bf-1384293786/mxbc-images/',
-    topBanners: ['banner-1.gif', 'banner-2.jpg', 'banner-3.gif', 'banner-4.gif', 'banner-5.gif', 'banner-6.gif'],
-  },
 
+  },
 
   /**
    * 生命周期函数--监听页面加载
